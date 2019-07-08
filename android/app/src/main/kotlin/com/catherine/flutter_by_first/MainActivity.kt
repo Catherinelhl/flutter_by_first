9996ba1cc48a444fafa5f3bae883bd5ac3396567
catherineliu <catherineliu@orangeblock.tech>
2019-07-08 17:11:40 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/catherine/flutter_by_first/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.catherine.flutter_by_first
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
